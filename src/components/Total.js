64796191e34a924e86a516872f44838e3d3b4c0d
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-02 05:48:52 -0500

    Improved readibility of the code

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Total = ({ parts }) => {
-    const callbackFn = (sum, part) => sum + part.exercises
-    const total = parts.reduce(callbackFn, 0)
+    const totalExercises = (sum, part) =>
+        (sum + part.exercises)
+
+    const total = parts.reduce(totalExercises, 0)
 
     return (
         <p><strong>total of {total} exercises</strong></p>

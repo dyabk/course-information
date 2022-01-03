34b751ddea5fd0985af360e1423369718f261196
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-02 23:27:17 -0500

    Added a separate top-level header

--- src/App.js
+++ src/App.js
@@ -49,8 +49,9 @@ const App = () => {
 
   return (
     <div>
-       {courses.map(course =>
-          <Course key={course.id} course={course} />)}
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />)}
     </div>
   )
 }
